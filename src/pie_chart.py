fd2af85bb4b71facef67cff43b5608e0ab3bb92a
Riya Eliza <riyaelizashaju@gmail.com>
2024-04-04 10:09:03 +0400

    Chart rendered when deployed, now fixing filtering function

--- src/pie_chart.py
+++ src/pie_chart.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, callback, Output, Input
+from dash import Dash, dcc, callback, Output, Input, html
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
 import altair as alt
@@ -11,15 +11,11 @@ df = pd.read_csv('data/filtered/pie_chart_data.csv')
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Layout
-# app.layout = dbc.Container([
-#     dvc.Vega(id='pie-chart', spec={}),
-#     dcc.Dropdown(id='year', options=df["REF_DATE"], value='2016'),
-#     dcc.Dropdown(id='industry', options=df["Industry"], value='Finance')
-# ])
-
 app.layout = dbc.Container([
     dvc.Vega(id='pie-chart', spec={}),
+    html.Label('Select a Year'),
     dcc.Dropdown(id='year', options=[{'label': year, 'value': year} for year in df["REF_DATE"].unique()], value='2016'),
+    html.Label('Select an Industry'),
     dcc.Dropdown(id='industry', options=[{'label': industry, 'value': industry} for industry in df["Industry"].unique()], value='Finance')
 ])
 
@@ -32,14 +28,19 @@ app.layout = dbc.Container([
 
 def create_chart(year, industry):
 
-    chart_df = df[(df["Industry"] == industry) &
-                     (df["REF_DATE"] == year)]
-    return(
-        alt.Chart(chart_df).mark_arc().encode(
-            theta="VALUE",
-            color="Gender"
-        )
+    # chart_df = df[(df["Industry"] == industry) &
+    #                  (df["REF_DATE"] == year)]
+    
+    # if chart_df.empty:
+    #     return {}
+    
+    # Create Altair chart specification
+    chart = alt.Chart(df).mark_arc().encode(
+        theta="VALUE",
+        color="Gender"
     )
+
+    return chart.to_dict()
 
 
 # Run the app/dashboard

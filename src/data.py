858047406ce9ab720339816d18870278eb7ccc99
SCOUT MCKEE <scoutmckee01@gmail.com>
2024-04-12 13:30:39 -0700

    reading in geo data

--- src/data.py
+++ src/data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import geopandas as gpd
+
 
 df = pd.read_csv("data/raw/filtered_canada.csv")
 
@@ -13,3 +15,4 @@ new_df.to_csv('data/filtered/filtered_data.csv', index=False)
 
 df = pd.read_csv('data/filtered/filtered_data.csv')
 
+canadian_provinces = gpd.read_file('data/filtered/map_chart_data.geojson')

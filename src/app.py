cbe499835563e8bec3ab2c74eeaaa8ec324bc3a7
SCOUT MCKEE <scoutmckee01@gmail.com>
2024-04-11 19:49:17 -0700

    now reading in data from data.py file

--- src/app.py
+++ src/app.py
@@ -5,21 +5,7 @@ import altair as alt
 import pandas as pd
 import plotly.graph_objs as go
 
-
-df = pd.read_csv("data/raw/filtered_canada.csv")
-
-new_df = df[(df["Size of enterprise"] == "Total all sizes") &
-            (df["Unit of measure"] == "Number") &
-            (df["Executive"] == "All officers\xa0") &
-            (df["GEO"] != "Unclassified province or territory")
-].copy()
-new_df = new_df.loc[:, ["REF_DATE", "Gender", "GEO", "VALUE", "Industry", "Type of corporation"]]
-
-new_df.to_csv('data/filtered/filtered_data.csv', index=False)
-
-df = pd.read_csv('data/filtered/filtered_data.csv')
-
-
+from data import df
 
 
 # Initialize the app
